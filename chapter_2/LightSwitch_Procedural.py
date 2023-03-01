5fa28e44c49b544021a5c7a4ff58c550558ccfd2
Matheus Zakken <matheus@marcogabriel.com.br>
2023-03-01 11:52:58 -0300

    procedural light_switch

---
+++ chapter_2/LightSwitch_Procedural.py
@@ -0,0 +1,18 @@
+def turnOn():
+    global switchIsOn
+
+    switchIsOn = True
+
+def turnOff():
+    global switchIsOn
+
+    switchIsOn = False
+
+switchIsOn = False
+print(switchIsOn)
+turnOn()
+print(switchIsOn)
+turnOff()
+print(switchIsOn)
+turnOn()
+print(switchIsOn)
